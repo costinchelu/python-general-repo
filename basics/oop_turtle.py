b04f7914f8a4383bb6c0443c780c04952e28d23f
costin <costin.chelu@gmail.com>
2025-07-05 21:00:47 +0300

    1.0.8

--- basics/oop_turtle.py
+++ basics/oop_turtle.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-def main():
+def turtle_example():
     timmy = Turtle()
     print(timmy)
     timmy.shape("turtle")
@@ -11,6 +12,18 @@ def main():
     print(my_screen.canvheight)
 
     my_screen.exitonclick()
+
+
+def prettytable_example():
+    pokemon_table = PrettyTable()
+    pokemon_table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+    pokemon_table.add_column("Type", ["Electric", "Water", "Fire"])
+    pokemon_table.align = "l"
+    print(pokemon_table)
+
+
+def main():
+    prettytable_example()
 
 
 if __name__ == "__main__":
