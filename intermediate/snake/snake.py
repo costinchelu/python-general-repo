2b630d3d56c94ce184a3367822f5cd41279ce40d
costin.chelu <costin.chelu@johnlewis.co.uk>
2025-07-08 17:44:20 +0300

    1.0.14

--- intermediate/snake/snake.py
+++ intermediate/snake/snake.py
@@ -17,12 +17,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         """First move the body, then the head will move only 20 so it will not create gaps between turtles"""
