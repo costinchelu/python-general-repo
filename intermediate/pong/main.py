2f0e150fb195daa211d3d7f1ad7f4b7cfe92527b
costin <costin.chelu@gmail.com>
2025-07-09 17:13:42 +0300

    1.0.15

--- intermediate/pong/main.py
+++ intermediate/pong/main.py
@@ -1,4 +1,4 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 from scoreboard import Scoreboard
@@ -16,13 +16,14 @@ ball = Ball()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
+# screen.onkey(r_paddle.go_up, "Up")
+# screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -31,16 +32,16 @@ while game_is_on:
         ball.bounce_y()
 
     #Detect collision with paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
         ball.bounce_x()
 
     #Detect R paddle misses
-    if ball.xcor() > 380:
+    if ball.xcor() > 350:
         ball.reset_position()
         scoreboard.l_point()
 
     #Detect L paddle misses:
-    if ball.xcor() < -380:
+    if ball.xcor() < -350:
         ball.reset_position()
         scoreboard.r_point()
 
