51fbf4e2f147f35eff95c7a2012bfb9dc40dfa0e
costin <costin.chelu@gmail.com>
2025-07-09 17:13:57 +0300

    1.0.16

--- intermediate/pong/main.py
+++ intermediate/pong/main.py
@@ -16,8 +16,8 @@ ball = Ball()
 scoreboard = Scoreboard()
 
 screen.listen()
-# screen.onkey(r_paddle.go_up, "Up")
-# screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
