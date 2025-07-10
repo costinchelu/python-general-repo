2b48272469c5189eb02aaaa796c1f9f9c8da25d7
costin.chelu <costin.chelu@johnlewis.co.uk>
2025-07-10 15:53:12 +0300

    1.0.18

--- intermediate/snake/scoreboard.py
+++ intermediate/snake/scoreboard.py
@@ -2,13 +2,23 @@ from turtle import Turtle
 
 ALIGN = "center"
 FONT = ("Courier", 24, "normal")
+FILE = "data.txt"
+
+def read_highscore():
+    with open(FILE) as file:
+        highscore = file.read()
+    return int(highscore)
+
+def write_highscore(highscore):
+    with open(FILE, mode="w") as file:
+        file.write(f"{highscore}")
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = read_highscore()
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -30,4 +40,6 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            write_highscore(self.highscore)
         self.score = 0
+        self.update_scoreboard()
