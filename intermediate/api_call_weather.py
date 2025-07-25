deb9b7e3d6a2843e45e8e0a4c4321ecc26a44785
costin <costin.chelu@gmail.com>
2025-07-25 21:15:53 +0300

    1.35.1

--- intermediate/api_call_weather.py
+++ intermediate/api_call_weather.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-#OPEN_WEATHER_API_KEY=ba0af3420d76bb626eb6c485aa5e9316
-OPEN_WEATHER_API_KEY = os.environ.get()
+
+OPEN_WEATHER_API_KEY = os.environ.get("OPEN_WEATHER_API_KEY")
 OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast"
 NO_OF_ENTRIES = 4
 GET_CALL_PARAMS = {
@@ -17,8 +17,8 @@ response = requests.get(url=OPEN_WEATHER_URL, params=GET_CALL_PARAMS)
 response.raise_for_status()
 response_data = response.json()
 
-intervals = response_data["list"]
 rain_intervals = []
+intervals = response_data["list"]
 for interval in intervals:
     if interval["weather"][0]["id"] < 700:
         rain_intervals.append(interval["dt_txt"])
