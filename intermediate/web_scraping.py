2a6b54e37dd95abd0d2258bd407c6210d7239785
costin.chelu <costin.chelu@johnlewis.co.uk>
2025-08-01 18:45:56 +0300

    1.41.0

---
+++ intermediate/web_scraping.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import pprint as pp
+
+site = "https://news.ycombinator.com/news"
+response = requests.get(url=site)
+yc_web_page = response.text
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+all_title_spans = soup.find_all(name="span", class_="titleline")
+all_title_anchor_tags = []
+for span in all_title_spans:
+    all_title_anchor_tags.append(span.find(name="a"))
+
+all_article_upvote_spans = soup.find_all(name="span", class_="score")
+all_article_upvote_elements = []
+for span in all_article_upvote_spans:
+    all_article_upvote_elements.append(span.getText())
+
+titles_list = []
+links_list = []
+upvote_list = []
+for tag in all_title_anchor_tags:
+    titles_list.append(tag.getText())
+    links_list.append(tag.get("href"))
+
+for upvote in all_article_upvote_elements:
+    up = int(upvote.split(" ")[0])
+    upvote_list.append(up)
+
+
+result = [titles_list, links_list, upvote_list]
+pp.pprint(result)
+
