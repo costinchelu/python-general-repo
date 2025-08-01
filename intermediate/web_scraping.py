1f0ede99725639389fb6f6582539c85de2eb82db
costin.chelu <costin.chelu@gmail.com>
2025-08-02 01:53:06 +0300

    1.48.0

--- intermediate/web_scraping.py
+++ intermediate/web_scraping.py
@@ -3,8 +3,10 @@ import requests
 import pprint as pp
 
 site = "https://news.ycombinator.com/news"
-response = requests.get(url=site)
-yc_web_page = response.text
+robots = "https://news.ycombinator.com/robots.txt"
+
+# pp.pprint(requests.get(robots).text)
+yc_web_page = requests.get(url=site).text
 soup = BeautifulSoup(yc_web_page, "html.parser")
 
 all_title_spans = soup.find_all(name="span", class_="titleline")
@@ -15,7 +17,7 @@ for span in all_title_spans:
 all_article_upvote_spans = soup.find_all(name="span", class_="score")
 all_article_upvote_elements = []
 for span in all_article_upvote_spans:
-    all_article_upvote_elements.append(span.getText())
+    all_article_upvote_elements.append(span.get_text())
 
 titles_list, links_list, upvote_list = [], [], []
 for tag in all_title_anchor_tags:
