12f3775b5e0eb70a350681b9a94d966ee259adbf
costin.chelu <costin.chelu@gmail.com>
2025-08-04 10:45:09 +0300

    1.48.2

--- intermediate/using_selenium.py
+++ intermediate/using_selenium.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
 
+# to keep the browser window open after the end of the program:
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(url="https://google.com")
